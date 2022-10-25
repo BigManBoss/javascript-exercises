66ceb990182f597200ae546e67f852d4d5abe91f
BigManBoss <teseopoma@gmail.com>
2022-10-25 18:22:32 +0200

    Exercise 2 complete

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+//    let text = string;
+//    let n = num;
+    let exitText = "";
+    if (num < 0) {
+        exitText = "ERROR";
+    };
+    while(num > 0) {
+        exitText += string;
+        num --;
+    };
+    return exitText;
 };
 
 // Do not edit below this line
