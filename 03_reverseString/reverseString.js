14bc90f73d9334e1964100d34227783bbb9af02b
BigManBoss <teseopoma@gmail.com>
2022-10-25 18:35:50 +0200

    exercise 3 complete

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(input) {
+    let invInput = ""
+    for (let i = input.length - 1; i >= 0; i--) {
+        invInput += input[i];
+    };
+    return invInput;
 };
 
 // Do not edit below this line
