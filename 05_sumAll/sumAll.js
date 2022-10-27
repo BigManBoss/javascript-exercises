acababa03ae53a18cf5089588d453161688b38a8
BigManBoss <teseopoma@gmail.com>
2022-10-27 19:51:50 +0200

    exercise 5 completed

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+    let sum = 0;
+    if(a >= 0 && b >= 0 && typeof a === "number" && typeof b === "number") {
+        if(a > b) {
+            while(b <= a) {
+                sum += b;
+                b++;
+            };
+        } else {
+            while(a <= b) {
+                sum += a;
+                a++;
+            };
+        };
+    } else {
+        sum = "ERROR";
+    };
+    return sum;
 };
 
 // Do not edit below this line
