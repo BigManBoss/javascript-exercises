83c6e19cbaa6d62f7d9853dc22d478bcc1e63618
BigManBoss <teseopoma@gmail.com>
2022-10-27 19:24:39 +0200

    exercise 4 cmpleted

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...cac) {
+    for (const c in cac) {
+        let remove = cac[c];
+        for (let i = 0;i < array.length; i++) {
+            if (array[i] === remove) {
+                array.splice(i, 1);
+            };
+        };
+    };
+    return array;
 };
 
 // Do not edit below this line
