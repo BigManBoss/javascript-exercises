d312ddb77fd1cebfcd97138080bd1c56a9167083
BigManBoss <teseopoma@gmail.com>
2022-10-29 17:08:57 +0200

    exercise 6 completed

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,19 @@
-const leapYears = function() {
-
+const leapYears = function(num) {
+    let result;
+    if (num % 4 === 0) {
+        if (num % 100 === 0) {
+            if (num % 400 === 0){
+                result = true;
+            } else {
+                result = false;
+            };
+        } else {
+            result = true;
+        };
+    } else {
+        result = false;
+    };
+    return result;
 };
 
 // Do not edit below this line
