bc2d48daa755a7cfedceac723a233f4f806c956c
BigManBoss <teseopoma@gmail.com>
2022-10-29 20:28:28 +0200

    exercise 8 completed

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,49 @@
-const add = function() {
-	
+const add = function(a, b) {
+	let sum = a + b;
+  return sum;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	let sub = a - b;
+  return sub;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  let sum = 0;
+	for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+  };
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  let mult = 1;
+//	for (let i = 0; i < arr.length; i++) {
+  for (let i in arr) {
+    mult *= arr[i];
+  };
+  return mult;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	let pow = 1;
+  for (let i = b; i > 0; i--) {
+    pow *= a
+  };
+  return pow;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	let res;
+  if (a === 0) {
+    res = 1;
+  } else {
+    res = 1;
+    for (let i = a; i > 0; i--){
+      res *= i
+    };
+  };
+  return res;
 };
 
 // Do not edit below this line
